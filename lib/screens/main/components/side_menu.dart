9822276bd62ab9d89150980525e11ad199ffe6aa
hakandindis <hakandindis@gmail.com>
2022-03-13 00:21:09 +0300

    knowledges added,download cv button added, social media icons added

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -5,6 +5,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:portfolio_app/constants.dart';
 import 'package:portfolio_app/screens/main/components/area_info_text.dart';
+import 'package:portfolio_app/screens/main/components/knowledges.dart';
 import 'package:portfolio_app/screens/main/components/my_info.dart';
 import 'package:portfolio_app/screens/main/components/skills.dart';
 
@@ -31,16 +32,53 @@ class SideMenu extends StatelessWidget {
                   Skills(),
                   SizedBox(height: defaultPadding),
                   Coding(),
+                  Knowledges(),
                   Divider(),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: defaultPadding),
-                    child: Text("Knowledge",
-                        style: Theme.of(context).textTheme.subtitle2),
+                  SizedBox(height: defaultPadding / 2),
+                  TextButton(
+                    onPressed: () {},
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          Text(
+                            "DOWNLOAD CV",
+                            style: TextStyle(
+                              color:
+                                  Theme.of(context).textTheme.bodyText1!.color,
+                            ),
+                          ),
+                          SizedBox(width: defaultPadding / 2),
+                          SvgPicture.asset("assets/icons/download.svg"),
+                        ],
+                      ),
+                    ),
                   ),
-                  Row(children: [
-                    SvgPicture.asset("assets/icons/check.svg"),
-                  ],),
+                  Container(
+                    margin: EdgeInsets.only(top: defaultPadding / 2),
+                    color: Color(0xFF2424E),
+                    child: Row(
+                      children: [
+                        Spacer(),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/linkedin.svg"),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/github.svg"),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/twitter.svg"),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset("assets/icons/linkedin.svg"),
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
